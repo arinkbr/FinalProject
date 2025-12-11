9f10968f517c44fbff254b9109ce9cacf4216c75
arin.kbr <arin.bornil@gmail.com>
2025-12-10 22:23:30 -0500

    Add method to check the validity of the postal code.

--- src/main/java/org/arin/Address.java
+++ src/main/java/org/arin/Address.java
@@ -23,5 +23,25 @@ public class Address {
         PE,
         SK,
     }
+
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() != 6) {
+            return false;
+        }
+
+        for (int i = 0; i < 6; i++) {
+            char c = postalCode.charAt(i);
+
+            if (i % 2 == 0) {
+                if (!Character.isLetter(c))
+                    return false;
+            } else {
+                if (!Character.isDigit(c))
+                    return false;
+            }
+        }
+
+        return true;
+    }
 }
 
