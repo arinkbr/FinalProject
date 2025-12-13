3a29181811f9d0a6ae4ec6bd1e69446fa3e2c1b1
arin.kbr <arin.bornil@gmail.com>
2025-12-12 19:57:40 -0500

    Add the method isDepartmentNameValid to check if a department name is valid or not. Meaning the name should only contain letters or space.

--- src/main/java/org/arin/Department.java
+++ src/main/java/org/arin/Department.java
@@ -7,4 +7,18 @@ public class Department {
     private String departmentName;
 
     private static int nextId = 1;
+
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null || departmentName.isBlank()) {
+            return false;
+        }
+
+        for (char c : departmentName.toCharArray()) {
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+
+        return true;
+     }
 }
