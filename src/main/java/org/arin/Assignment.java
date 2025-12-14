48535b38d643d16a59d38fd7d38aa589dad50cda
arin.kbr <arin.bornil@gmail.com>
2025-12-14 00:20:25 -0500

    Add the constructor for every field as well as a counting loop so we can create an empty score slot for each student so the scores list always matches the number of registered students before grades are assigned. Add a static field called assignmentAvg to store the calculated average score of the assignment.

--- src/main/java/org/arin/Assignment.java
+++ src/main/java/org/arin/Assignment.java
@@ -8,8 +8,21 @@ public class Assignment {
     private String assignmentId;
     private String assignmentName;
     private double weight;
-    private int maxScore;
     private ArrayList<Integer> scores;
 
     private static int nextId = 1;
+    private double assignmentAvg;
+
+    public Assignment(String assignmentName, double weight, int maxScore, int numOfStudent) {
+        this.assignmentId = String.format("A%03d", nextId++);
+        this.assignmentName = assignmentName;
+        this.weight = weight;
+        this.scores = new ArrayList<>();
+
+        for (int i = 0; i < numOfStudent; i++) {
+            scores.add(null);
+        }
+
+        this.assignmentAvg = 0.0;
+    }
 }
