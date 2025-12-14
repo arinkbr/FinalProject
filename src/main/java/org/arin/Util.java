6e9aa45b39a873c37a55536807470be65f7573f8
arin.kbr <arin.bornil@gmail.com>
2025-12-14 09:11:47 -0500

    Add a toTitleCase method in order to put the desired names in the correct format in the other classes.

--- src/main/java/org/arin/Util.java
+++ src/main/java/org/arin/Util.java
@@ -1,4 +1,21 @@
 package org.arin;
 
 public class Util {
+
+    public static String toTitleCase(String str) {
+        if (str == null)
+            return null;
+
+        String[] words = str.trim().split(" ");
+        String result = "";
+
+        for (String word : words) {
+            if (word.isEmpty())
+                continue;
+
+            result += word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase() + " ";
+        }
+
+        return result.trim();
+    }
 }
